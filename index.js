0a066a951fc41d1af9279d27329714624074acbe
rohan-haque <rohanhaque234@gmail.com>
2023-05-03 11:54:27 +0600

    json data loaded

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
-
 const app = express();
+const port = 5000;
+// const cors = require('cors')
+
+const recipe = require('./chefs.json')
+// app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send("Hello from server")
+    res.send("Server is running")
 })
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000")
+app.get('/recipe', (req, res) => {
+    res.send(recipe)
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port : ", port)
 })
